164976411a74676a386fd0ae57512063cb2c622a
Natalia Bezhenar <natalia.bezhenar@gmail.com>
2021-12-20 17:17:46 +0200

    Searchbar refactor

--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,51 +1,49 @@
 import "./Searchbar.css";
-import { Component } from "react";
+import { useState } from "react";
 import { toast } from "react-toastify";
 import PropTypes from "prop-types";
 import { ImSearch } from "react-icons/im";
 import "react-toastify/dist/ReactToastify.css";
 
-export default class Searchbar extends Component {
-  state = { searchQuery: "" };
+export default function Searchbar({ onSubmit }) {
+  const [searchQuery, setSearchQuery] = useState("track");
 
-  handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    if (this.state.searchQuery.trim() === "") {
+    if (searchQuery.trim() === "") {
       toast("Please enter search query");
       return;
     }
-    this.props.onSubmit(this.state.searchQuery);
+    onSubmit(searchQuery);
   };
 
-  handleQueryChange = (e) => {
-    this.setState({ searchQuery: e.currentTarget.value.toLowerCase() });
+  const handleQueryChange = (e) => {
+    setSearchQuery(e.currentTarget.value.toLowerCase());
   };
 
-  render() {
-    return (
-      <header className="Searchbar">
-        <form className="SearchForm" onSubmit={this.handleSubmit}>
-          <button
-            type="button"
-            className="SearchForm-button"
-            onClick={this.handleSubmit}
-          >
-            <ImSearch />
-            <span className="SearchForm-button-label">Search</span>
-          </button>
-          <input
-            className="SearchForm-input"
-            type="text"
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-            value={this.state.searchQuery}
-            onChange={this.handleQueryChange}
-          />
-        </form>
-      </header>
-    );
-  }
+  return (
+    <header className="Searchbar">
+      <form className="SearchForm" onSubmit={handleSubmit}>
+        <button
+          type="button"
+          className="SearchForm-button"
+          onClick={handleSubmit}
+        >
+          <ImSearch />
+          <span className="SearchForm-button-label">Search</span>
+        </button>
+        <input
+          className="SearchForm-input"
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+          value={searchQuery}
+          onChange={handleQueryChange}
+        />
+      </form>
+    </header>
+  );
 }
 
 Searchbar.propTypes = {
